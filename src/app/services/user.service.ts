2a867191b18438f59c7fce4c1f24ff7241777354
Kwntax <sinyc.gfx@gmail.com>
2022-11-29 16:40:32 -0800

    changes the final part 2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,9 +5,13 @@ interface User{
   name: string;
   age: number;
   email: string;
+  profile_picture: string;
+  is_active: number;
   password: string;
   phonenumber: number;
-
+  partner_id: number;
+  created_at: Date;
+  updated_at: Date;
 }
 
 @Injectable({
